9646db4119b8f0327094aa757715bcca23efa9bf
Fieldry <wangzx@stu.pku.edu.cn>
2026-04-03 17:17:54 +0800

    fix(ehrflowbench): make wrapper entrypoints executable

--- data/ehrflowbench/scripts/curate_generated_tasks.py
+++ data/ehrflowbench/scripts/curate_generated_tasks.py
@@ -3,4 +3,7 @@ import sys as _sys
 
 
 _impl = _import_module("data.ehrflowbench.scripts.prepare_tasks.curate_generated_tasks")
-_sys.modules[__name__] = _impl
+if __name__ == "__main__":
+    _impl.main()
+else:
+    _sys.modules[__name__] = _impl
